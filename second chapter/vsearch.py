e7f15acaa658aa2311ddf898298c0393d9d08516
gakawarstone <pinnekerv@gmail.com>
2018-07-20 20:27:13 +0600

    eighth commit

--- second chapter/vsearch.py
+++ second chapter/vsearch.py
@@ -1,4 +1,8 @@
 def search4vowels(phrase:str) -> set:
     #""" Return vowels pinned in aforementioned phrase. """#
     vowels = set('aeiou')
-    return = vowels.intersection(set(phrase))
+    return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str) -> set:
+    #""" Return array of letters from 'letters', pinned in aforementioned phrase. """#
+    return set(letters).intersection(set(phrase))
