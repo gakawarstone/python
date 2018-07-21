375a31e31bef61a3811115a68b1d5d3b095d3924
gakawarstone <pinnekerv@gmail.com>
2018-07-21 08:50:52 +0600

    17'th commit

--- mymodules/vsearch/setup.py
+++ mymodules/vsearch/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(
     name = 'vsearch',
+    version = '1.1',
     py_modules = ['vsearch'],
 )
--- mymodules/vsearch/vsearch.py
+++ mymodules/vsearch/vsearch.py
@@ -1,8 +1,10 @@
-def search4vowels(phrase:str) -> set:
-    #""" Return vowels pinned in aforementioned phrase. """#
+def search4vowels(phrase: str) -> set:
+    """Return vowels pinned in aforementioned phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
 
-def search4letters(phrase:str, letters:str = 'aeiou') -> set:
-    #""" Return array of letters from 'letters', pinned in aforementioned phrase. """#
+
+def search4letters(phrase: str, letters: str='aeiou') -> set:
+    """Return array of letters from 'letters',
+    pinned in aforementioned phrase."""
     return set(letters).intersection(set(phrase))
