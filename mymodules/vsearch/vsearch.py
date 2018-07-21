93e9dfd7bfc0dca2adc942bd9242a35b35af73ef
gakawarstone <pinnekerv@gmail.com>
2018-07-21 09:38:37 +0600

    19'th commit 19'th commit

--- mymodules/vsearch/vsearch.py
+++ mymodules/vsearch/vsearch.py
@@ -1,4 +1,4 @@
-def search4vowels(phrase: str) -> set:
+ def search4vowels(phrase: str) -> set:
     """Return vowels pinned in aforementioned phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
