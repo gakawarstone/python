a1044ea9612655ffee8b476bd80a6c41bc5a496e
gakawarstone <pinnekerv@gmail.com>
2018-07-21 13:59:45 +0600

    63'th commit

--- flask/vsearch4web/vsearch4web.py
+++ flask/vsearch4web/vsearch4web.py
@@ -10,10 +10,16 @@ def hello() -> str:
 
 
 @app.route('/search4', methods=['post'])
-def do_search() -> str:
+def do_search() -> 'html':
     phrase = request.form['phrase']
     letters = request.form['letters']
-    return str(search4letters(phrase, letters))
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results,)
 
 
 @app.route('/entry')
