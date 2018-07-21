45ba961076e6c69d46f14dcf32e12d8f432719e4
gakawarstone <pinnekerv@gmail.com>
2018-07-21 13:03:37 +0600

    32'th commit

--- flask/vsearch4web/flask.py
+++ flask/vsearch4web/flask.py
@@ -18,7 +18,7 @@ def do_search() -> str:
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html',
-                            the_title='Welcome to search4letters on the web!')
+                           the_title='Welcome to search4letters on the web!')
 
 
 app.run()
