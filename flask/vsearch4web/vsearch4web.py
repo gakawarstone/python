ec893a32f45e68dcbae81f21efed6b3e4931cc1a
gakawarstone <pinnekerv@gmail.com>
2018-07-21 18:29:43 +0600

    42'th commit

--- flask/vsearch4web/vsearch4web.py
+++ flask/vsearch4web/vsearch4web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, escape
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
     with open('vsearch.log', 'a') as log:
-        print(req, res, file=log)
+        print(req.form, file=log, end='|')
+        print(req.remote_addr, file=log, end='|')
+        print(req.user_agent, file=log, end='|')
+        print(res, file=log)
 
 
 @app.route('/search4', methods=['post'])
@@ -34,7 +37,7 @@ def entry_page() -> 'html':
 def view_the_log() -> str:
     with open('vsearch.log') as log:
         contents = log.read()
-    return contents
+    return escape(contents)
 
 
 if __name__ == '__main__':
