d97b53d1fcd1844a68d66635a09c6b2bda06dc81
gakawarstone <pinnekerv@gmail.com>
2018-07-21 20:00:35 +0600

    45'th commit

--- flask/vsearch4web/vsearch4web.py
+++ flask/vsearch4web/vsearch4web.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as lr:
-        print(req.form, req.remote_addr, req.user_agent, res, file=lr, sep='|')
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr,
+              req.user_agent, res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['post'])
@@ -31,14 +32,18 @@ def entry_page() -> 'html':
 
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     contents = []
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
             for item in line.split('|'):
                 contents[-1].append(escape(item))
-    return str(contents)
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':
