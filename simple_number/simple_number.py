378dbc395fb80cd99a289cd708dd7b905cc9ef7a
gakawarstone <pinnekerv@gmail.com>
2020-01-08 17:43:05 +0500

    81 commit

--- simple_number/simple_number.py
+++ simple_number/simple_number.py
@@ -1,16 +1,13 @@
 from math import sqrt
 
 
-def siple(x):
+def simple(x):
     k = int(sqrt(x))
+    if x == 1:
+        return False
+    if x == 2:
+        return True
     for i in range(k):
         if x % (i + 2) == 0:
-            return 0
-    return 1
-
-
-def main():
-    print(siple(int(input())))
-
-
-main()
+            return False
+    return True
