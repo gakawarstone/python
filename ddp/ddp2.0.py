98f410c0819872baf2ed82b99162408b52d98c7c
gakawarstone <pinnekerv@gmail.com>
2020-01-10 17:26:51 +0500

    83 commit

--- ddp/ddp2.0.py
+++ ddp/ddp2.0.py
@@ -12,27 +12,33 @@ def number(x, mass):
         return mass[i]
 
 
-
 def check(mass):
     len = len(mass)
-
 
 
 def post(x, y):
     mass = {}
     check = {}
     massive(x, mass)
+    check = np.zeros(y, int)
     n = 0
     k = 0
     r = 0
     c = 0
+    i = 0
     for i in range(x):
         c = y - 1
         check[c] = mass[i]
         k = check[c]
         if i == x - 1:
-            i += 1
+            check[c - 1] += 1
+            n = check[c - 1]
+            i = 0
+        if n == x - 1:
             c -= 1
+        if k < x - 1 and c != y - 1:
+            c += 1
+        print(k)
     return r
 
 
