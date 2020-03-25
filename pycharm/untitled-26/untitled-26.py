74905db3f4cd23dffaef946c9627bbd1f5b947c1
gakawarstone <pinnekerv@gmail.com>
2020-03-25 15:58:52 +0500

    101 commit

--- pycharm/untitled-26/untitled-26.py
+++ pycharm/untitled-26/untitled-26.py
@@ -9,9 +9,35 @@ def update_dictionary(d, key, value):
 
 
 def read(file_name):
-    str = ''
+    d = {}
     with open(file_name) as inp:
         for line in inp:
             list_line = [i for i in line.strip().split(';')]
-            for item in list_line:
-                update_dictionary(d, list_line[0], item)
+            for item in range(1, len(list_line)):
+                if item == 0:
+                    continue
+                update_dictionary(d, list_line[0], int(list_line[item]))
+    return d
+
+
+def dict_output(d):
+    for key in d:
+        print(key, ':', sep='', end=' ')
+        print(d[key])
+
+
+def list_middle(l):
+    sum = 0
+    for i in range(len(l)):
+        sum += l[i]
+    middle = round(sum / len(l), 9)
+    return middle
+
+
+def main():
+    d = read('input.txt')
+    dict_output(d)
+    print(list_middle(d['s']))
+
+
+main()
