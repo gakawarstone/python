87ce730265c848d50d9058d31c0cfee2b99d0e98
gakawarstone <pinnekerv@gmail.com>
2020-04-03 17:27:06 +0500

    105 commit

--- pycharm/football.py
+++ pycharm/football.py
@@ -6,7 +6,7 @@ def update_dictionary(d, key, value, mode='list'):
             d[key] = 0
             d[key] += 1
 
-    if mode == 'list':
+    elif mode == 'list':
         if key in d:
             d_key = d[key]
             d_key.append(value)
@@ -14,6 +14,18 @@ def update_dictionary(d, key, value, mode='list'):
             d[key] = []
             d_key = d[key]
             d_key.append(value)
+
+    else:
+        if key in d:
+            d_key = d[key]
+            if mode in d_key:
+                d_key[mode] += value
+            else:
+                d_key[mode] = value
+        else:
+            d[key] = {}
+            d_key = d[key]
+            d_key[mode] = value
 
 
 def dict_output(d):
@@ -27,8 +39,17 @@ def read(file_name):
     with open(file_name) as inp:
         for line in inp:
             l_str = line.strip().split(';')
-            update_dictionary(d, l_str[0], 1, 'list_up')  # cnt matches team 1
-            update_dictionary(d, l_str[2], 1, 'list_up')  # cnt matches team 2
+            update_dictionary(d, l_str[0], 1, 'cnt')  # cnt matches team 1
+            update_dictionary(d, l_str[2], 1, 'cnt')  # cnt matches team 2
+            if l_str[1] > l_str[3]:
+                update_dictionary(d, l_str[0], 1, 'win')
+                update_dictionary(d, l_str[2], 1, 'lose')
+            elif l_str[1] < l_str[3]:
+                update_dictionary(d, l_str[0], 1, 'lose')
+                update_dictionary(d, l_str[2], 1, 'win')
+            else:
+                update_dictionary(d, l_str[0], 1, 'draw')
+                update_dictionary(d, l_str[2], 1, 'draw')
     return d
 
 
