6fd02cfdac7916c3594ff7faf5da72c54bef1820
gakawarstone <pinnekerv@gmail.com>
2020-04-03 17:50:14 +0500

    106 commit

--- pycharm/football.py
+++ pycharm/football.py
@@ -34,22 +34,50 @@ def dict_output(d):
         print(d[key])
 
 
+def chart(d, l_str):
+        team_1 = l_str[0]
+        team_2 = l_str[2]
+        scr_team_1 = l_str[1]
+        scr_team_2 = l_str[3]
+        update_dictionary(d, team_1, 1, 'cnt')  # cnt matches team 1
+        update_dictionary(d, team_2, 1, 'cnt')  # cnt matches team 2
+        if scr_team_1 > scr_team_2:
+            update_dictionary(d, team_1, 1, 'win')
+            update_dictionary(d, team_1, 0, 'lose')
+            update_dictionary(d, team_1, 0, 'draw')
+            update_dictionary(d, team_2, 0, 'win')
+            update_dictionary(d, team_2, 1, 'lose')
+            update_dictionary(d, team_2, 0, 'draw')
+        elif scr_team_1 < scr_team_2:
+            update_dictionary(d, team_1, 0, 'win')
+            update_dictionary(d, team_1, 1, 'lose')
+            update_dictionary(d, team_1, 0, 'draw')
+            update_dictionary(d, team_2, 1, 'win')
+            update_dictionary(d, team_2, 0, 'lose')
+            update_dictionary(d, team_2, 0, 'draw')
+        else:
+            update_dictionary(d, team_1, 0, 'win')
+            update_dictionary(d, team_1, 0, 'lose')
+            update_dictionary(d, team_1, 1, 'draw')
+            update_dictionary(d, team_2, 0, 'win')
+            update_dictionary(d, team_2, 0, 'lose')
+            update_dictionary(d, team_2, 1, 'draw')
+
+
+def points(d):
+    for key in d:
+        d_key = d[key]
+        team_points = d_key['win'] * 3 + d_key['draw']
+        update_dictionary(d, key, team_points, 'points')
+
+
 def read(file_name):
     d = {}
     with open(file_name) as inp:
         for line in inp:
             l_str = line.strip().split(';')
-            update_dictionary(d, l_str[0], 1, 'cnt')  # cnt matches team 1
-            update_dictionary(d, l_str[2], 1, 'cnt')  # cnt matches team 2
-            if l_str[1] > l_str[3]:
-                update_dictionary(d, l_str[0], 1, 'win')
-                update_dictionary(d, l_str[2], 1, 'lose')
-            elif l_str[1] < l_str[3]:
-                update_dictionary(d, l_str[0], 1, 'lose')
-                update_dictionary(d, l_str[2], 1, 'win')
-            else:
-                update_dictionary(d, l_str[0], 1, 'draw')
-                update_dictionary(d, l_str[2], 1, 'draw')
+            chart(d, l_str)
+        points(d)
     return d
 
 
