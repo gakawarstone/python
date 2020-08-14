2415907dc94424d466b1558ee44f05be694e8891
gakawarstone <pinnekerv@gmail.com>
2020-08-14 21:02:38 +0600

    126 commit

--- pygame/img.py
+++ pygame/img.py
@@ -1,4 +1,4 @@
 import pygame
-jojo = pygame.image.load('jojo1.jpg')
-dio = pygame.image.load('dio.jpg')
-real_dio = pygame.image.load('real_dio.jpg')
+jojo = pygame.image.load('img/jojo1.jpg')
+dio = pygame.image.load('img/dio.jpg')
+real_dio = pygame.image.load('img/real_dio.jpg')
