1ba64f1bc55fb31c94906f59a3d88c9cd7d910dc
gakawarstone <pinnekerv@gmail.com>
2020-08-15 14:57:19 +0600

    132 commit

--- pygame/chapter_sprite.py
+++ pygame/chapter_sprite.py
@@ -12,3 +12,6 @@ def sprite():
     game.start()
     game.screen_new(chapter_name, img.school500, 'Я спрайт', img.sprite500)
     game.screen_new(chapter_name, img.school500, text.down_1, img.down_1, -30)
+    game.screen_new(chapter_name, img.school500, text.down_2, img.down_2)
+    game.screen_new(chapter_name, img.school500, text.down_3, img.down_2)
+    game.screen_new(chapter_name, img.school500, text.down_4, img.down_3)
