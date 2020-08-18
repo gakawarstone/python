c086180e0507fdcbdf48b710567d75cbcc4a8873
gakawarstone <pinnekerv@gmail.com>
2020-08-18 21:14:54 +0600

    144 commit

--- pygame/img.py
+++ pygame/img.py
@@ -1,9 +1,14 @@
 import pygame
+# chapter dio at chapter_dio.py
 jojo = pygame.image.load('img/jojo1.jpg')
 dio = pygame.image.load('img/dio.jpg')
 real_dio = pygame.image.load('img/real_dio.jpg')
 start_bg = pygame.image.load('img/start_bg.png')
+
+# start screen background
 bg_2 = pygame.image.load('img/bg_2.png')
+
+# chapter grant at chapter_sprite.py
 school = pygame.image.load('img/school.png')
 sprite_1 = pygame.image.load('img/sprite_1.png')
 school500 = pygame.image.load('img/school500.png')
@@ -12,6 +17,8 @@ mylogo = pygame.image.load('img/mylogo')
 down_1 = pygame.image.load('img/down_1.png')
 down_2 = pygame.image.load('img/down_2.png')
 down_3 = pygame.image.load('img/down_3.png')
+
+# chapter new dio at chapter_dio.py
 jojo500 = pygame.image.load('img/jojo500.jpg')
 dio500 = pygame.image.load('img/dio500.jpg')
 real_dio500 = pygame.image.load('img/real_dio500.jpg')
