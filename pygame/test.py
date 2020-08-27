3594201121a2a2d1a7f1232628b41d2315d9f67e
gakawarstone <pinnekerv@gmail.com>
2020-08-27 22:23:55 +0600

    175 commit

--- pygame/test.py
+++ pygame/test.py
@@ -1,14 +1,14 @@
 import pygame
 import gkt
 import game_h as game
+import chapter_dio as dio
+from chapter_sprite import grant
 pygame.init()
 pygame.display.set_caption('GK')
 
-first = game.Frame()
-second = game.Frame()
-print(first.uid, second.uid)
-print(game.Frame.id_list)
-Frame = game.Frame
-print(Frame.get_from_id(0))
+dio.dio_new()
+grant()
+fr = game.Frame.get_from_id(0)
+fr.show()
 
 pygame.quit()
