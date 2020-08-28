5bb73532fd289557ded6e12d13c31450bfbc365d
gakawarstone <pinnekerv@gmail.com>
2020-08-28 12:14:51 +0600

    180 commit

--- pygame/game_h.py
+++ pygame/game_h.py
@@ -201,42 +201,6 @@ def start():
     time.sleep(1)
 
 
-def screen(chapter_name, img_, text, sprt='', bg=img.bg_2):
-    run = True
-    while run:
-        pygame.time.delay(10)
-
-        if_close()
-
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_c]:
-            splash()
-        if keys[pygame.K_d]:
-            run = False
-
-        c.win.fill(cl.dark_white)
-        draw_img(0, 0, bg)
-        gm_print('Chapter: ' + chapter_name, 30, (10, 10))
-        gm_print(text, 15, (12, 332))
-        draw_img(10, 50, img_)
-        if sprt:
-            draw_img(200, 50, sprt)
-        pygame.draw.rect(c.win, cl.black, (10, 330, 480, 100), 1)
-
-        pygame.draw.rect(c.win, cl.black, (10, 440, 170, 50), 1)
-        gm_print('PREVIOS', 30, (27, 450))
-        pygame.draw.rect(c.win, cl.black, (190, 440, 120, 50), 1)
-        gm_print('SAVE', 30, (210, 450))
-        next = Button((320, 440), (170, 50))
-        next.hover()
-        pygame.draw.rect(c.win, cl.black, (320, 440, 170, 50), 1)
-        gm_print('NEXT', 30, (370, 450))
-        if next.click():
-            run = False
-        pygame.display.update()
-    time.sleep(1)
-
-
 def frame(img_, text, sprt_=None, sprt_x=None):
     gf = Frame()
     gf.background = img_
@@ -248,7 +212,7 @@ def frame(img_, text, sprt_=None, sprt_x=None):
     gf.show()
 
 
-def screen_(chapter_name, img_, text, sprt_=None, bg=img.bg_2, sprt_x=100):
+def screen(chapter_name, img_, text, sprt_=None, bg=img.bg_2, sprt_x=100):
     gf = Screen()
     gf.background = bg
     gf.text = text
