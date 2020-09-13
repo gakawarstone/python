7835c26a95106d825c90d744de32f51273e01248
gakawarstone <pinnekerv@gmail.com>
2020-09-13 15:44:25 +0600

    203 commit

--- neuro/simple_neuro.py
+++ neuro/simple_neuro.py
@@ -28,5 +28,37 @@ class Neuron():
         self.list_out_links.append(weight)
 
 
+class Network():
+    def __init__(self):
+        self.layers_list = []
+
+    def create_layer(self, neurons_number):
+        self.layers_list.append([Neuron() for i in range(neurons_number)])
+
+    def link_layers(self):
+        for i in range(len(self.layers_list) - 1):
+            for j in range(len(self.layers_list[i])):
+                for k in range(len((self.layers_list[i + 1]))):
+                    self.layers_list[i][j].link()
+
+    def count(self):
+        for i in range(len(self.layers_list) - 1):
+            for j in range(len(self.layers_list[i])):
+                for k in range(len((self.layers_list[i + 1]))):
+                    n = self.layers_list[i][j].get_value()
+                    weigth = self.layers_list[i][j].list_out_links[k]
+                    self.layers_list[i+1][k].give_value(n * weigth)
+
+
 def activate(x):
     return 1 / (1 + np.exp(-x))
+
+
+nw = Network()
+nw.create_layer(1)
+nw.layers_list[0][0].give_value(1)
+nw.create_layer(4)
+nw.create_layer(1)
+nw.link_layers()
+nw.count()
+print(nw.layers_list[2][0].get_value())
