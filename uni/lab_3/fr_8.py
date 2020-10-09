88622011f255509029790b718ff5307b350294e6
gakawarstone <pinnekerv@gmail.com>
2020-10-09 09:24:03 +0600

    209 commit

--- uni/lab_3/fr_8.py
+++ uni/lab_3/fr_8.py
@@ -1,5 +1,6 @@
 numbers_oct = [input() for i in range(2)]
 numbers = list(map(lambda x: int(x, 8), numbers_oct))
 print(sum(numbers))
-numbers[-1] = - numbers[-1]
-print(sum(numbers))
+numbers_plus = [numbers[i] for i in range(len(numbers)) if i % 2 == 0]
+numbers_minus = [-numbers[i] for i in range(len(numbers)) if i % 2 == 1]
+print(sum(numbers_plus) + sum(numbers_minus))
