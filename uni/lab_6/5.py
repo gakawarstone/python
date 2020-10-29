6e0a56f498d6f98393f9995c6a938d4868511cc0
gakawarstone <pinnekerv@gmail.com>
2020-10-29 09:03:27 +0600

    214 commit

--- uni/lab_6/5.py
+++ uni/lab_6/5.py
@@ -1,7 +1,7 @@
 numbers = [int(input()) for i in range(6)]
 logic = False
 
-for i in range(len(numbers) - 1):
+for i in range(len(numbers)):
     if numbers[i] + numbers[i-1] == 8:
         logic = True
 
